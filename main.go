a5326f9ebd95f3edd5b177869eeb386b56557fe3
Georgy Buranov <gburanov@gmail.com>
2017-09-14 20:16:08 +0200

    Added influx

--- main.go
+++ main.go
@@ -1,11 +1,27 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"math/rand"
+	"time"
+
+	asyncinflux "github.com/applift/async-influxdb-client"
+	"github.com/joho/godotenv"
+)
+
+var metricService *asyncinflux.AsyncClient
 
 func main() {
-	events := make(chan int)
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
 
-	go func() { events <- 2 }()
+	events := make(chan int)
+	go func() { generateEvents(events) }()
+
+	initClients()
 
 	for {
 		responceTime := <-events
@@ -13,8 +29,24 @@ func main() {
 	}
 }
 
-func generateEvent()
+func initClients() {
+	// Setup defaults
+	var err error
+	metricService, err = asyncinflux.DefaultClient()
+	if err != nil {
+		log.Fatal(err.Error())
+	}
+}
+
+func generateEvents(events chan<- int) {
+	for {
+		events <- int(rand.Int31n(10))
+		time.Sleep(time.Second * 2)
+	}
+}
 
 func logResponce(time int) {
 	fmt.Printf("Response took %d seconds\n", time)
+
+	metricService.Send(asyncinflux.NewMetricDatum("test_measurement", map[string]string{}, map[string]interface{}{"response": time}))
 }
